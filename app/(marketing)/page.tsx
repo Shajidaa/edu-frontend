373c34b5b3b28ccefbcd989cc88bfddf1b0c6fc5
Shajidaa <shajidaakterlopa101@gmail.com>
2026-02-24 09:18:57 +0600

    update the floder structer

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,18 @@
+import AudienceSection from "./components/home/AudienceSection";
+import Banner from "./components/home/Banner";
+import FeatureSection from "./components/home/FeatureSection";
+import LessonFeature from "./components/home/LessonFeature";
+import Statistics from "./components/home/Statistics";
+
+
+export default function Home() {
+  return (
+    <>
+    <Banner/>
+    <Statistics/>
+    <FeatureSection/>
+    <LessonFeature/>
+    <AudienceSection/>
+        </>
+  );
+}
