f80f52de917bac8d85225dcb7cd5d4a420fd3ec8
Shajidaa <shajidaakterlopa101@gmail.com>
2026-03-08 20:57:59 +0600

    summer page created

--- app/(kids)/kidsForCoding/camps/summer/page.tsx
+++ app/(kids)/kidsForCoding/camps/summer/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CampCard from '../_components/campCard'
+import SummerHeroSection from '../_components/summerHeroSection'
 
 export default function SummerCamp() {
   return (
-    <div>SummerCamp</div>
+    <>
+<SummerHeroSection/>
+      <CampCard />
+    </>
   )
 }
